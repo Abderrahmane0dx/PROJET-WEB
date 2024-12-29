266af5c0665a6cdd2a1be05dd4e43e41e62819bc
Abderrahmane0dx <benamaraabderrahmane2@gmail.com>
2024-12-29 17:57:10 +0100

    FIXING THE DISPLAY OF PRODUCTS IN THE HOME PAGE

--- JS/client.js
+++ JS/client.js
@@ -1,18 +1,15 @@
+//****************VARIABALES****************//
+let productContainerWidth = document.querySelector('.product-cards-container').offsetWidth;
+let productCardWidth = 260;
+let productCardsPerRow = Math.floor(productContainerWidth / (productCardWidth + 10));
+let marginSpacing = ((productContainerWidth - (productCardsPerRow * productCardWidth)) / (productCardsPerRow - 1)) - 5;
+let lastElement = productCardsPerRow - 1;
+let productCards;
+let productSections = document.querySelectorAll('.product-section');
 
 document.addEventListener('DOMContentLoaded', () =>{
 
   //****************PRODUCT-CARDS-DISPLAY****************//
-
-  //****************VARIABALES****************//
-  let productContainerWidth = document.querySelector('.product-cards-container').offsetWidth;
-  let productCardWidth = 260;
-  let productCardsPerRow = Math.floor(productContainerWidth / (productCardWidth + 10));
-  let marginSpacing = ((productContainerWidth - (productCardsPerRow * productCardWidth)) / (productCardsPerRow - 1)) - 5;
-  let lastElement = productCardsPerRow - 1;
-  let productCards;
-  let productSections = document.querySelectorAll('.product-section');
-
-
   productSections.forEach((section) =>{
     productCards = section.querySelectorAll('.product-card');
     for(let i = 0; i < productCardsPerRow; i++){
@@ -35,6 +32,54 @@ document.addEventListener('DOMContentLoaded', () =>{
     });
   })
   
+})
 
-  
+//SLIDSHOWPART:
+let slideshowButtons = document.querySelectorAll('.slideshow-buttons');
+slideshowButtons.forEach((button) => {
+   button.addEventListener('click',() =>{
+    let slideshowSection = button.parentElement.dataset.slideshow;
+    console.log(slideshowSection);
+    let slideshowContainer = document.querySelector(`#product-section-${slideshowSection}`)
+    console.log(slideshowContainer);
+    let productCards = slideshowContainer.querySelectorAll('.product-card');
+    console.log(slideshowContainer);
+
+    if(button.classList.contains('prev-button')){
+      if(lastElement <= (productCardsPerRow - 1)){
+        lastElement = productCards.length - 1;
+      }
+      else{
+        lastElement--
+      }
+    }
+    else if (button.classList.contains('next-button')){
+      if(lastElement === (productCards.length - 1)){
+        lastElement = productCardsPerRow - 1;
+      }
+      else{
+        lastElement++
+      }
+    }
+    else{
+        console.log("SlideShow: ERROR Occured");
+    }
+
+    for(let i=0; i<productCards.length; i++){
+        if((i <= lastElement) && (i >=(lastElement-(productCardsPerRow - 1)))){
+            productCards[i].classList.add('active');
+
+            if(i === lastElement){
+                productCards[i].style.marginRight = '0px'
+            }
+            else{
+                productCards[i].style.marginRight = `${marginSpacing}px`
+            }
+        }
+        else{
+            productCards[i].classList.remove('active');
+            productCards[i].style.marginRight = '0px'
+        }
+    }
+   })
 })
